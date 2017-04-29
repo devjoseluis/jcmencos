5b6e4d3020e6341afecdbc40ef5e6849e46e75ec
José Luis <dev.joseluis@gmail.com>
2017-04-28 20:43:44 -0600

    - Add nav dropdown
    - Add menu items

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 // components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
+// UIkit.notification('Hello world.');
 
 
-var stickys = UIkit.navbar('#navigation');
+// var stickys = UIkit.navbar('#navigation');
 
 Vue.config.productionTip = false
 
